f095dab55f868b1a553f35cdfcc4a78e9d062c1e
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-06 15:35:21 -0400

    Spring Actuator

---
+++ src/main/java/com/sam/sp10_springactuator/Sp10SpringActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.sam.sp10_springactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp10SpringActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp10SpringActuatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/sp10_springactuator/Sp10SpringActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.sp10_springactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp10SpringActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
